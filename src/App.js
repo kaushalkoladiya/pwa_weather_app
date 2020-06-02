a2d4ab4e59e9f9e22b8a5bf09e0558ed5dca7068
Kaushal <50080954+kausHal789@users.noreply.github.com>
2020-06-02 20:41:02 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div></div>;
-}
+import { fetchWeather } from "./api/fetchWeather";
+
+const App = () => {
+  const [term, setTerm] = useState("");
+  const [weather, setWeather] = useState({});
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(term);
+      console.log(data);
+      setWeather(data);
+      setTerm("");
+    }
+  };
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={term}
+        onChange={(e) => setTerm(e.target.value)}
+        onKeyPress={search}
+      />
+
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;c</sup>
+          </div>
+          <div className="min-max-temp">
+            {Math.round(weather.main.temp_min)}
+            <sup>&deg;c</sup> - {Math.round(weather.main.temp_max)}
+            <sup>&deg;c</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
